0b3edcaad36efd9619a7cb3ba833a49acdbff689
greevedogg <greevedogg@gmail.com>
2015-11-19 11:39:33 -0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from forms import LuggageForm
 
 app = Flask(__name__) 
@@ -10,13 +10,17 @@ def luggage():
   form = LuggageForm()
   return render_template('luggage.html', form=form)
   
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-  form = ContactForm()
+@app.route('/luggage', methods=['GET', 'POST'])
+def luggage():
+  form = LuggageForm()
  
   if request.method == 'POST':
-    return 'Form posted.'
+   if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('luggage.html', form=form)
+   else:
+    return 'Entry Submitted to Luggage Log.'
  
   elif request.method == 'GET':
-    return render_template('contact.html', form=form)  
+    return render_template('luggage.html', form=form)  
   
