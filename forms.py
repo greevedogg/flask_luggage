0559563ae68fab2527c840cfdeca4aa622dbab85
greevedogg <greevedogg@gmail.com>
2015-11-19 11:53:51 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask.ext.wtf import Form, TextField, SubmitField, validators, ValidationError
  
 class LuggageForm(Form):
-  name = TextField("Last Name", [validators.Required()])
-  ticket = TextField("Ticket #", [validators.Required()])
-  location = TextField("Location", [validators.Required()])
-  bagCount = TextAreaField("Bag Count", [validators.Required()])
+  name = TextField("Last Name", [validators.Required("Please enter a Last Name")])
+  ticket = TextField("Ticket #", [validators.Required("Please enter a ticket #")])
+  location = TextField("Location", [validators.Required("Please enter a location")])
+  bagCount = TextAreaField("Bag Count", [validators.Required("Please enter # of bags")])
   submit = SubmitField("Store")
