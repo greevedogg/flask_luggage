1c1468cf91717810c435c73de727e81486737468
greevedogg <greevedogg@gmail.com>
2015-11-27 21:22:38 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,8 +11,26 @@ class Luggage(db.Model):
     loggedIn = db.Column(db.String(30), db.ForeignKey('user.id'))
     timeIn = time(now(%H:%M))
     
+    def __init__(self, name, ticket, location, bagCount, loggedIn, timeIn):
+        self.name = name
+        self.ticket = ticket
+        self.location = location
+        self.bagCount = bagCount
+        self.loggedIn = loggedIn
+        self.timeIn = timeIn
+    
+    def __repr__(self):
+        ??????????????
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(30), unique=True)
     pin = db.Column(db.String(10), unique=True)
+    
+    def __intit__(self, userName, pin):
+        self.userName = userName
+        self.pin = pin
+        
+    def __repr__(self):
+        ??????????????????
     
