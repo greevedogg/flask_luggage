38b6d5eee5c0fdb5aaab337eebed188af11b7061
greevedogg <greevedogg@gmail.com>
2015-12-05 15:32:37 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+from wtforms import Form, TextField, SubmitField, validators, ValidationError
  
 class LuggageForm(Form):
   name = TextField("Last Name", [validators.Required("Please enter a Last Name")])
   ticket = TextField("Ticket #", [validators.Required("Please enter a ticket #")])
   location = TextField("Location", [validators.Required("Please enter a location")])
-  bagCount = TextAreaField("Bag Count", [validators.Required("Please enter # of bags")])
+  bagCount = TextField("Bag Count", [validators.Required("Please enter # of bags")])
   submit = SubmitField("Store")
