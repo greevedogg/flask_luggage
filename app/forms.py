a8ac63611d99a9b62435751f8597f1b5046ecdaa
guilleconstantino <guilleconstantino@gmail.com>
2017-01-18 17:35:18 +0100

    Done task #2: Location code validation

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from wtforms import Form, StringField, TextAreaField, SubmitField, HiddenField, validators, ValidationError
 from wtforms.validators import DataRequired
 from models import Luggage
-
+from string import lower
+import re
 
 def already_exists(form, field):
     if not form.id.data and Luggage.query.filter_by(ticket=field.data).first():
@@ -13,10 +14,16 @@ def check_last_modified(form, field):
         raise ValidationError('Please enter your initials, only 2 or 3 letters')
 
 
+def check_location(form, field):
+    exceptionsAllowed = ["corral", "cart", "desk"]
+    if exceptionsAllowed.count(lower( field.data )) == 0 and not re.search("^[0-9]+[A-Za-z-\']+$", field.data):
+        raise ValidationError('Location has to start with numbers and ends with letters. Exceptions: Corral, Cart, Desk')
+
+
 class LuggageForm(Form):
   name = StringField("Last Name", [validators.Required("Please enter a Last Name"), validators.Regexp(r"[A-Za-z-\']+", message="Please include only letters")])
   ticket = StringField("Ticket #", [validators.Required("Please enter a Ticket #"), already_exists])
-  location = StringField("Location", [validators.Required("Please enter a Location")])
+  location = StringField("Location", [validators.Required("Please enter a Location"), check_location])
   # bagCount = StringField("Bag Count", [validators.Required("Please enter # of bags"), validators.Regexp(r"\d{1,2}$", message="Please include only numbers. Maximum of two digits.")])
   bagCount = StringField("Bag Count", [validators.Required("Please enter # of Bags")])
   comments = TextAreaField("Comments")
